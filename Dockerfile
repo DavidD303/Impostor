b5de90fe581884278e3b49e1f319b2daef06d279
aPinat <apinat@apinat.de>
2020-10-13 16:47:48 +0200

    Add Dockerfile, docker-compose, move constants.ts emotes to env

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:latest AS build-client
+WORKDIR /build
+COPY ./client client
+COPY ./Hazel-dotNETCore Hazel-dotNETCore
+WORKDIR /build/client
+RUN dotnet publish -r linux-x64 --self-contained true -c release -o /out
+
+FROM node:latest AS build-bot
+WORKDIR /build
+COPY ./bot .
+RUN npm install
+RUN ./node_modules/.bin/tsc
+
+FROM node:latest
+LABEL org.opencontainers.image.source=https://github.com/aPinat/Impostor
+WORKDIR /app
+COPY --from=build-client /out ./client
+COPY --from=build-bot /build ./bot
+ENV AU_CLIENT_DIR /app/client
+RUN npm install -g knex
+WORKDIR /app/bot
+CMD sleep 5 && knex migrate:latest && node ./dist/index.js
--- bot/src/constants.ts
+++ bot/src/constants.ts
@@ -23,33 +23,33 @@ export const SHORT_REGION_NAMES = {
 };
 
 export const COLOR_EMOTES: { [key: number]: string } = {
-    [0]: "crewmate_red:761211569635459092",
-    [1]: "crewmate_blue:761211569546985502",
-    [2]: "crewmate_green:761211569744904232",
-    [3]: "crewmate_pink:761211569379999806",
-    [4]: "crewmate_orange:761211569606361127",
-    [5]: "crewmate_yellow:761211569950294046",
-    [6]: "crewmate_black:761211569597710356",
-    [7]: "crewmate_white:761211569950162985",
-    [8]: "crewmate_purple:761211569282744332",
-    [9]: "crewmate_brown:761211569467818015",
-    [10]: "crewmate_cyan:761211569744510976",
-    [11]: "crewmate_lime:761211569555636226",
+    [0]: process.env.crewmate_red!,
+    [1]: process.env.crewmate_blue!,
+    [2]: process.env.crewmate_green!,
+    [3]: process.env.crewmate_pink!,
+    [4]: process.env.crewmate_orange!,
+    [5]: process.env.crewmate_yellow!,
+    [6]: process.env.crewmate_black!,
+    [7]: process.env.crewmate_white!,
+    [8]: process.env.crewmate_purple!,
+    [9]: process.env.crewmate_brown!,
+    [10]: process.env.crewmate_cyan!,
+    [11]: process.env.crewmate_lime!,
 };
 
 export const DEAD_COLOR_EMOTES: { [key: number]: string } = {
-    [0]: "crewmate_red_dead:761987905370980372",
-    [1]: "crewmate_blue_dead:761987905375174676",
-    [2]: "crewmate_green_dead:761987905358397470",
-    [3]: "crewmate_pink_dead:761987905363116052",
-    [4]: "crewmate_orange_dead:761987905392345098",
-    [5]: "crewmate_yellow_dead:761987905391951923",
-    [6]: "crewmate_black_dead:761987905362984970",
-    [7]: "crewmate_white_dead:761987905082097705",
-    [8]: "crewmate_purple_dead:761987905244889119",
-    [9]: "crewmate_brown_dead:761987905500610628",
-    [10]: "crewmate_cyan_dead:761987905459322901",
-    [11]: "crewmate_lime_dead:761987905064402985",
+    [0]: process.env.crewmate_red_dead!,
+    [1]: process.env.crewmate_blue_dead!,
+    [2]: process.env.crewmate_green_dead!,
+    [3]: process.env.crewmate_pink_dead!,
+    [4]: process.env.crewmate_orange_dead!,
+    [5]: process.env.crewmate_yellow_dead!,
+    [6]: process.env.crewmate_black_dead!,
+    [7]: process.env.crewmate_white_dead!,
+    [8]: process.env.crewmate_purple_dead!,
+    [9]: process.env.crewmate_brown_dead!,
+    [10]: process.env.crewmate_cyan_dead!,
+    [11]: process.env.crewmate_lime_dead!,
 };
 
 export const BOT_INVITE_LINK =
@@ -60,7 +60,7 @@ export const COLOR_EMOTE_IDS = Object.values(COLOR_EMOTES).map(x => x.split(":")
 export const EMOTE_IDS_TO_COLOR: { [key: string]: number } = {};
 Object.entries(COLOR_EMOTES).forEach(x => (EMOTE_IDS_TO_COLOR[x[1].split(":")[1]] = +x[0]));
 
-export const GROUPING_DISABLED_EMOJI = "<:impostor_grouping_disabled:761985590123954176>";
-export const GROUPING_ENABLED_EMOJI = "<:impostor_grouping_enabled:761985589784215565>";
-export const GROUPING_TOGGLE_EMOJI = "toggle_impostor_grouping:761987527493943326";
-export const LEAVE_EMOJI = "leave_lobby:762751494495666207";
+export const GROUPING_DISABLED_EMOJI = process.env.GROUPING_DISABLED_EMOJI!;
+export const GROUPING_ENABLED_EMOJI = process.env.GROUPING_ENABLED_EMOJI!;
+export const GROUPING_TOGGLE_EMOJI = process.env.GROUPING_TOGGLE_EMOJI!;
+export const LEAVE_EMOJI = process.env.LEAVE_EMOJI!;
